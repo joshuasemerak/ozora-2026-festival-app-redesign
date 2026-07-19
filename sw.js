da9c588d0dcbba1b9871630b807d29204eed20d3
joshuasemerak <161644148+joshuasemerak@users.noreply.github.com>
2026-07-19 16:56:58 +0100

    Refresh offline cache for curated comments

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="ozora-2026-redesign-v9";
+const CACHE="ozora-2026-redesign-v10";
 const APP_SHELL=["./index.html","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",event=>{
   event.waitUntil((async()=>{
