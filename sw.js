05562e910f67318fde628144f5f3992c88e4403d
joshuasemerak <161644148+joshuasemerak@users.noreply.github.com>
2026-07-21 19:21:18 +0100

    Restore official map hotspots

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="ozora-2026-redesign-v19";
+const CACHE="ozora-2026-redesign-v20";
 const APP_SHELL=["./index.html","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",event=>{
   event.waitUntil((async()=>{
