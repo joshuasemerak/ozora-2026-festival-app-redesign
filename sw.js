0e8b857fdb5f559f964e4a4ac5850c04a7a51744
joshuasemerak <161644148+joshuasemerak@users.noreply.github.com>
2026-07-21 23:02:36 +0100

    Restore curated Picks and add festival route

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="ozora-2026-redesign-v22";
+const CACHE="ozora-2026-redesign-v23";
 const APP_SHELL=["./index.html","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",event=>{
   event.waitUntil((async()=>{
