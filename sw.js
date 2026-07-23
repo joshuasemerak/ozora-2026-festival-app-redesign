c3132a4fb7a701fca3d5c50d15d323d5d446ec9a
joshuasemerak <joshuasemerak@protonmail.com>
2026-07-23 20:10:33 +0100

    Add onboarding and quick rating controls

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="ozora-2026-redesign-v26";
+const CACHE="ozora-2026-redesign-v27";
 const APP_SHELL=["./index.html","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",event=>{
   event.waitUntil((async()=>{
